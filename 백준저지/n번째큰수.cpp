a3d7efd45ccf3562bf2e8c467678efb3758a331a
jerry <wogkr101@gmail.com>
2021-02-15 21:01:46 +0900

    21/02/15

---
+++ 백준저지/n번째큰수.cpp
@@ -0,0 +1,20 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//priority_queue<int> q;
+//
+//int main() {
+//	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//	int n;
+//	cin >> n;
+//	vector<int> v;
+//	for (int i = 0; i < n * n; i++) {
+//		int a;
+//		cin >> a;
+//		q.push(-a);
+//		if (q.size() > n) {
+//			q.pop();
+//		}
+//	}
+//	cout << -q.top();
+//}
