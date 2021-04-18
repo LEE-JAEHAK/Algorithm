c32222a734f63ffd87d73161bd38fa712d90a8c3
LEE-JAEHAK <wogkr101@gmail.com>
2021-04-18 20:42:31 +0900

    21/04/18

---
+++ 백준저지/소스4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1 2 3 4 5 6 7 8 9 0
+// 2 5 5 4 5 6 3 7 6 6
+// 2 3 4 5 6 7 
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+
+	}
+}
